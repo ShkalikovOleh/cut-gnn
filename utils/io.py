48251e43cd76548463cdd77b022d22882052e10e
Oleh Shkalikov <Shkalikov.Oleh@gmail.com>
2023-06-04 21:21:43 +0200

    Add nx io utils

---
+++ utils/io.py
@@ -0,0 +1,23 @@
+import numpy as np
+import networkx as nx
+from networkx.readwrite.gml import literal_destringizer
+
+import pathlib
+import typing
+
+path_type = typing.Union[str, pathlib.Path]
+
+def write_graph(graph: nx.Graph, path: path_type) -> None:
+    nx.write_gml(graph, path, stringizer=np.array2string)
+
+def read_graph(path: path_type) -> nx.Graph:
+    def str_to_array(string):
+        if '[' in string:
+            string = string.replace('[','')
+            string = string.replace(']','')
+            res = np.fromstring(string, sep=' ')
+            return res
+        else:
+            return literal_destringizer(string)
+
+    return nx.read_gml(path, destringizer=str_to_array)
