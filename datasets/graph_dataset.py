092e6b43c4accb3724db3f6c7d5d37c8f7621b68
Oleh Shkalikov <Shkalikov.Oleh@gmail.com>
2023-06-06 20:18:07 +0200

    Fix file sorting in GraphDataset

--- datasets/graph_dataset.py
+++ datasets/graph_dataset.py
@@ -4,17 +4,24 @@ from torch.utils.data import Dataset
 import os
 import glob
 import pickle
+import re
 
 from utils.io import read_graph, path_type
+
+# Source: https://stackoverflow.com/questions/4813061/non-alphanumeric-list-order-from-os-listdir
+def sorted_alphanumeric(data):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
+    return sorted(data, key=alphanum_key)
 
 class GraphDataset(Dataset):
     def __init__(self, root_path: path_type, load_cycles: bool = True) -> None:
         super().__init__()
 
-        self.__graph_files = sorted(glob.glob(os.path.join(root_path, '*.gml')))
+        self.__graph_files = sorted_alphanumeric(glob.glob(os.path.join(root_path, '*.gml')))
         self.load_cycles = load_cycles
         if load_cycles:
-            self.__cycles_files = sorted(glob.glob(os.path.join(root_path, '*.pkl')))
+            self.__cycles_files = sorted_alphanumeric(glob.glob(os.path.join(root_path, '*.pkl')))
 
     def __len__(self):
         return len(self.__graph_files)
