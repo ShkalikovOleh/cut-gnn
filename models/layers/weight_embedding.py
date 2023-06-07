a43501e18b71f892d937c8271502ea4b823e0f78
Oleh Shkalikov <Shkalikov.Oleh@gmail.com>
2023-06-07 21:16:41 +0200

    Add weight embeddings

---
+++ models/layers/weight_embedding.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+
+def min_max_norm_weights(weights: torch.Tensor):
+    r"""Mapping weights value into interval [-1, 1]
+    with use of min-max normalization
+    """
+
+    wmin, wmax = torch.min(weights), torch.max(weights)
+    weights = 2 * (weights - wmin) / (wmax - wmin) - 1
+    return weights
+
+class WeightEmbedding(nn.Module):
+    r"""Split weights into bins defined by boundaries and
+    assign learnable (usual PyTorch) embedding for every bin
+    """
+
+    def __init__(self, emb_dim: int, boundaries: torch.Tensor, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.emb = nn.Embedding(len(boundaries), emb_dim)
+        self.register_buffer('boundaries', boundaries)
+
+    def forward(self, weights) -> torch.Tensor:
+        idxs = torch.bucketize(weights, self.get_buffer('boundaries'))
+        return self.emb(idxs)
